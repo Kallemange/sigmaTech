31f66d5ccb185f7510d446776cbeab307f3ed313
Karl <karlmagnus.lundin@gmail.com>
2019-08-04 11:23:46 +0200

    1:st version. Functioning version with a problem description added in text

--- problemB/problemB.py
+++ problemB/problemB.py
@@ -2,69 +2,35 @@
 '''
 Solve a max # tests performed to get an I_max value
 Test will be performed by finding max amount of test needed.
-The method is iteratively updated, in versions
-    1) for a given constant step size, iterate through the k<n/2 and calculate x_max
-    as x=floor(n/k)+k
-    2) TBA
+The method is iteratively updated, in versions,
+BRUTE FORCE Solution
+1)
+Investigating the first 12 situations, we get a solution looking like:
+I_max:  1| 2| 3| 4| 5| 6| 7| 8| 9| 10| 11| 12|
+x:      0| 1| 2| 2| 3| 3| 3| 4| 4| 4 |  4| 5 |
+Indicating that the solution is a series where the number corresponds to an
+inductive sum such that the I_max <=1+2+...+m for some m. e.g. 1+2+3<8<1+2+3+4=10
+thus I_max=8-->4 tests
 
-IN sys.stdin/valuesfile
-int[]: read input until 0, terminate reading
-OUT sys.stdout/print
-int[]: minimal max number of tests for any series of max ampere value
 '''
+
 import math
 import sys
 import fileinput
 path="C:/Users/kalle/Documents/Sigma/sigmaTech/problemB/batteries.in"
 
-def calcMinForI(I):
-    print("known I: ", I)
-    x_best=10000
-    if I<2:
-        x_best=0
-    elif I==2:
-        x_best=1
-    else:
-        X=I
-        k=math.ceil(math.sqrt(I))
-        counter=0
-        print("Iteration ", counter+1, ", k=", k)
-        x_1=1+k
-        x_2=0
-        while k>2:
-            X-=k
-            k=math.ceil(math.sqrt(X))
-            x_2+=1
-            counter+=1
-            print("Iteration ", counter+1, " k=", k, "interval size: ", X, "max tests=", counter+k)
-            if counter>1000:
-                break
-        #print(x_2)
-        x_2+=k
-        #print(k)
-
-    '''
-        For a value of I_max, and an untested set of values 0<=I<=I_max, there is
-        an optimal step size, only related to the size of the interval. Each step
-        should be taken at that optimum, meaning the step size will change over
-        the iterations. E.g. for a known max value n, the set is X: 0<=I_max<n,
-        testing the value k gives either:
-            a) k>I_max ==> 0<=I_max<k
-            b) k<I_max ==> k<I_max<n
-        For a large interval X --> k>>1
-        For a small interval X --> k->1
-
-        We should find the value such that (n-1)/k=k <==> k=sqrt(X_max-X_min)
-        [still doesn't seem to be an optimal value, I need to investigate more]
-    '''
-    print(x_best)
-
-def calcMinTest(n):
-    for I in n:
-        k_min=calcMinForI(I)
+def calcMinForI():
+    I_max=4711
+    x=[0, 0]
+    i=0
+    while len(x)<I_max:
+        x+=[i]*i
+        i+=1
+    return x
 
 def main(path=0):
     values=[]
+    #Read from log file
     if path !=0:
         file=open(path, 'r')
         for line in file:
@@ -73,15 +39,18 @@ def main(path=0):
             else:
                 break
         file.close()
+    #Read from terminal (for kattis)
     else:
         for line in sys.stdin:
             if int(line)!=0:
                 values.append(int(line.rstrip('\n')))
             else:
                 break
-    calcMinTest(values)
-
+    series=calcMinForI()
+    values2=list(range(24))
+    for i in values:
+        print(series[i])
 
 if __name__ == "__main__":
-    #main(0)
-    main(path)
+    main(0)
+    #main(path)
