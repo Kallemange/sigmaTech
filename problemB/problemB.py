a380925452602e6f0a544f0c95cc21956588af3f
Karl <karlmagnus.lundin@gmail.com>
2019-08-04 08:37:06 +0200

    attmpt at solving problem with quadratic function, suspended for now

--- problemB/problemB.py
+++ problemB/problemB.py
@@ -7,38 +7,79 @@ The method is iteratively updated, in versions
     as x=floor(n/k)+k
     2) TBA
 
-IN sys.stdin/textfile
+IN sys.stdin/valuesfile
 int[]: read input until 0, terminate reading
 OUT sys.stdout/print
 int[]: minimal max number of tests for any series of max ampere value
 '''
-
+import math
 import sys
 import fileinput
 path="C:/Users/kalle/Documents/Sigma/sigmaTech/problemB/batteries.in"
 
+def calcMinForI(I):
+    print("known I: ", I)
+    x_best=10000
+    if I<2:
+        x_best=0
+    elif I==2:
+        x_best=1
+    else:
+        X=I
+        k=math.ceil(math.sqrt(I))
+        counter=0
+        print("Iteration ", counter+1, ", k=", k)
+        x_1=1+k
+        x_2=0
+        while k>2:
+            X-=k
+            k=math.ceil(math.sqrt(X))
+            x_2+=1
+            counter+=1
+            print("Iteration ", counter+1, " k=", k, "interval size: ", X, "max tests=", counter+k)
+            if counter>1000:
+                break
+        #print(x_2)
+        x_2+=k
+        #print(k)
+
+    '''
+        For a value of I_max, and an untested set of values 0<=I<=I_max, there is
+        an optimal step size, only related to the size of the interval. Each step
+        should be taken at that optimum, meaning the step size will change over
+        the iterations. E.g. for a known max value n, the set is X: 0<=I_max<n,
+        testing the value k gives either:
+            a) k>I_max ==> 0<=I_max<k
+            b) k<I_max ==> k<I_max<n
+        For a large interval X --> k>>1
+        For a small interval X --> k->1
+
+        We should find the value such that (n-1)/k=k <==> k=sqrt(X_max-X_min)
+        [still doesn't seem to be an optimal value, I need to investigate more]
+    '''
+    print(x_best)
+
 def calcMinTest(n):
-    return 0
+    for I in n:
+        k_min=calcMinForI(I)
 
 def main(path=0):
-    text=[]
+    values=[]
     if path !=0:
         file=open(path, 'r')
         for line in file:
             if int(line)!=0:
-                text.append(int(line.rstrip('\n')))
+                values.append(int(line.rstrip('\n')))
             else:
                 break
         file.close()
     else:
         for line in sys.stdin:
             if int(line)!=0:
-                text.append(int(line.rstrip('\n')))
+                values.append(int(line.rstrip('\n')))
             else:
                 break
-    #print(text)
-    #text=text.split()
-    print(calcMinTest(1))
+    calcMinTest(values)
 
 
 if __name__ == "__main__":
